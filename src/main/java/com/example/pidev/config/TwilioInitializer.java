b9426e8264c2f28cbc093fc11e3f8ea8833a72fc
unknown <ahmedamin.benhsouna@esprit.tn>
2023-05-08 21:42:23 +0200

    fix

--- src/main/java/com/example/pidev/config/TwilioInitializer.java
+++ src/main/java/com/example/pidev/config/TwilioInitializer.java
@@ -1,19 +1,26 @@
 package com.example.pidev.config;
 
 import com.twilio.Twilio;
-import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@AllArgsConstructor
 public class TwilioInitializer {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(TwilioInitializer.class);
 
     private final TwilioConfiguration twilioConfiguration;
 
+    @Autowired
+    public TwilioInitializer(TwilioConfiguration twilioConfiguration) {
+        this.twilioConfiguration = twilioConfiguration;
 
+        Twilio.init(
+                twilioConfiguration.getAccountSid(),
+                twilioConfiguration.getAuthToken()
+        );
+        LOGGER.info("Twilio initialized ... with account sid {} ", twilioConfiguration.getAccountSid());
+    }
 }
